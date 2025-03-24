d65bb3abfd2a8d73398173b026d19d55f9a3759c
Steven Woolston <steven.woolston@gmail.com>
2025-03-24 11:48:25 +1000

    Debugging

--- src/services/traveltek/index.ts
+++ src/services/traveltek/index.ts
@@ -429,9 +429,9 @@ export const doLogin = async (credentials: Credentials, browser, page) => {
 			await page.waitForSelector(loginButtonSelector, { timeout: 20000 });
 			await page.click(loginButtonSelector);
 			await page.evaluate(async () => {
-				await sleep(5000);
 				if (!document.querySelector(`input[name='username']`)) {
 					//	Login page has not simply reloaded due to failure
+					page.waitForNavigation({ waitUntil: 'networkidle0' });
 					return true;
 				}
 				throw new Error(`Login page re-loaded. Trying again.`);
